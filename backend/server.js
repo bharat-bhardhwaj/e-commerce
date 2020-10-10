81b8022c1ef4bfbe9b708fe127becda2a107f5be
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-10 17:05:04 +0530

    setup the mongodb connection and compass as well

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from  'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js'
 import products from './data/products.js';
 
+//o75IzEgM9iiQ2pvo
+
+
 dotenv.config()
+connectDB();
 const app=express();
 
 app.get('/', (req,res)=>{
