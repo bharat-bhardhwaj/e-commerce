7df589a535a760a0c3095ef7a28efb3dcbf1c415
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-10 10:35:44 +0530

    my first commit devlop navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h1>
+          <Container>Welcome to Broshop</Container>
+        </h1>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
