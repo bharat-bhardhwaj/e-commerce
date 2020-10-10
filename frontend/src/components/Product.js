9a443d64b8d188495617cf97abce53aa664c1e34
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-10 12:09:47 +0530

    created the rating component and dynamic stars

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -8,30 +9,26 @@ const Product = ({ product }) => {
         <a href={`/products/${product._id}`}>
           <Card.Img src={product.image} variant='top' />
         </a>
-     
 
-      <Card.Body>
-        <a href={`/products/${product._id}`}>
-          <Card.Title as='div' style={{fontSize:
-        '1.5rem'}}>
-            <>{product.name}</>
-          </Card.Title>
-        </a>
+        <Card.Body>
+          <a href={`/products/${product._id}`}>
+            <Card.Title as='div' style={{ fontSize: '1.5rem' }}>
+              <>{product.name}</>
+            </Card.Title>
+          </a>
 
+          <Card.Text as='h3'>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              
+            />
+          </Card.Text>
 
-        <Card.Text as='div'>
-            <div className='my-3'style={{fontSize:
-        '1.5rem'}}>
-                    {product.rating} from {product.numReviews} reviews
-            </div>
-        </Card.Text>
-
-
-        <Card.Text as='h3'>
-           <b>${product.price}</b> 
-        </Card.Text>
-      </Card.Body>
-
+          <Card.Text as='h3'>
+            <b>${product.price}</b>
+          </Card.Text>
+        </Card.Body>
       </Card>
     </>
   )
