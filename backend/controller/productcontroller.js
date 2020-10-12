abab681f43582170cc00eb0c5d7990919eaabefe
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-12 17:03:18 +0530

    added controller in the routes

---
+++ backend/controller/productcontroller.js
@@ -0,0 +1,33 @@
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModels.js';
+
+// @desc fetch all products
+// @route Get /api/products
+// @access Public
+const getProducts =asyncHandler(async(req,res) => {
+    const products = await Product.find({})
+  
+   
+    res.json(products)
+})
+
+// @desc fetch single products
+// @route Get /api/products/;id
+// @access Public
+const getProductById =asyncHandler(async(req,res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      res.json(product)
+    } else {
+
+      res.status(404)
+      throw new Error('Product not found')
+    }
+})
+
+
+export {
+    getProductById,
+    getProducts
+}
