0ddcdfc091c73cf9ee6da8ce134d4a67027b0032
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-15 22:58:54 +0530

    added the register routes

--- backend/middleware/erroMiddleware.js
+++ backend/middleware/erroMiddleware.js
@@ -1,6 +1,6 @@
 const notFound = (req,res,next) => {
-    res.status(404);
-    return new Error(`Not Found -- ${req.originalUrl}`)
+    // res.status(404);
+    // return new Error(`Not Found -- ${req.originalUrl}`)
     
     // next(error);
 }
