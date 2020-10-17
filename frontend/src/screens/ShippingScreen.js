217208a235c79a7b3ddcb3bf14009784b0624cd5
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-17 17:31:48 +0530

    added the navbar to shipping page

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,8 +2,10 @@ import React,{useState}from 'react'
 
 import { Form, Button} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
+import CheckoutSteps from '../components/CheckoutSteps';
 import FormContainer from '../components/FormContainer'
 import {saveShippingAddress} from '../actions/cartActions';
+
 
 
 
@@ -26,6 +28,7 @@ const ShippingScreen = ({history}) => {
         history.push('/payment');
     }
     return <FormContainer>
+      <CheckoutSteps step1 step2 />
         <h1>
             Shipping
         </h1>
