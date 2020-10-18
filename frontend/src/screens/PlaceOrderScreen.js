45178e417e156e6d30fa5ae9feef4bce085ef2af
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-18 08:14:39 +0530

    added the order to database funcitonality

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,12 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Fragment ,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, Image, Card, ListGroup } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderAction'
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({history}) => {
+  const dispatch=useDispatch();
   const cart = useSelector((state) => state.cart)
 
   //calculate prices
@@ -27,8 +29,30 @@ const PlaceOrderScreen = () => {
         Number(cart.taxPrice)
     ).toFixed(2)
   }
+
+  const orderCreate =useSelector(state => state.orderCreate);
+  const{order,success,error} =orderCreate;
+
+  useEffect(()=>{
+    if(success){
+      history.push(`/order/${order._id}`)
+      
+    }
+    // eslint-disable-next-line
+  },[history,success])
   const placeOrderHandle = () => {
-    console.log('order')
+    if(cart){
+      dispatch(createOrder({
+        orderItems:cart.cartItems,
+        shippingAddress:cart.shippingAddress,
+        paymentMethod:cart.paymentMethod,
+        itemsPrice:cart.itemsPrice,
+        shippingPrice:cart.shippingPrice,
+        taxPrice:cart.taxPrice,
+        totalPrice:cart.totalPrice
+
+      }))
+    }
   }
   return (
     <Fragment>
@@ -121,6 +145,9 @@ const PlaceOrderScreen = () => {
                   <Col>Total</Col>
                   <Col>${cart && cart.totalPrice}</Col>
                 </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && <Message variant='danger'>{error}</Message>}
               </ListGroup.Item>
               <ListGroup.Item>
                 <Button
