0dab2f8a8e19cc18485668436bfaaa0023598db8
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-20 20:48:08 +0530

    added the product list screen to admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen'
 
 
 const App = () => {
@@ -39,6 +40,7 @@ const App = () => {
             <Route path='/products/:id' component={ProductScreen} exact/>
             <Route path='/cart/:id?' component={CartScreen} exact/>
             <Route path='/admin/userlist' component={UserListScreen} exact/>
+            <Route path='/admin/productlist' component={ProductListScreen} exact/>
             <Route path='/admin/user/:id/edit' component={UserEditScreen} exact/>
             <Route path='/' component={HomeScreen} exact/>
       
