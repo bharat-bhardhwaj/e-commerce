a8fbe92f5bd3fa96c1af99161a9a858fda1821fe
Bharat Bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-10-22 11:09:55 +0530

    added the search products functionality

--- backend/controller/productcontroller.js
+++ backend/controller/productcontroller.js
@@ -5,7 +5,15 @@ import Product from '../models/productModels.js'
 // @route Get /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {}
+  const products = await Product.find({ ...keyword })
 
   res.json(products)
 })
@@ -101,27 +109,30 @@ const createProductReview = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
   if (product) {
-    const alreadyReviewed =product.reviews.find(r  => r.user.toString() ===req.user._id.toString())
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    )
 
-    if(alreadyReviewed){
+    if (alreadyReviewed) {
       res.status(400)
       throw new Error('Product already reviewed')
     }
 
-    const review ={
-      name:req.user.name,
-      rating:Number(rating),
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
       comment,
-      user:req.user._id
+      user: req.user._id,
     }
 
     product.reviews.push(review)
-    product.numReviews =product.reviews.length
-    product.rating =product.reviews.reduce((acc,item) => item.rating + acc ,0)/ product.reviews.length
+    product.numReviews = product.reviews.length
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
 
     await product.save()
-    res.status(201).json({message:'Review added'})
-
+    res.status(201).json({ message: 'Review added' })
   } else {
     res.status(404)
     throw new Error('Product not found')
@@ -133,5 +144,5 @@ export {
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview
+  createProductReview,
 }
